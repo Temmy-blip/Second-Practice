241c72c6a570e487f3126caeeeb2fb9c3ddc2b6c
Temmy-blip <tiwatopefunmi8@gmail.com>
2024-12-06 10:56:49 +0100

    Add files via upload

---
+++ Htlm 2/script.js
@@ -0,0 +1,16 @@
+
+// function testAlert (){
+//     alert("Javascript is ok");
+//     alert("Javascript is good");
+//     alert("Javascript is ook");
+// }
+// testAlert ();
+
+
+// function testAlert (message){
+//     alert(message);
+    
+// }
+// testAlert (123);
+// testAlert (5+2);
+console.log(2+7)
